7ebf4850b0a4ad062cea607db1e82536b2fea572
yukkuri-lab <tmatsukawa123@gmail.com>
2026-08-29 07:48:44 +0900

    Refresh tic-tac-toe cache version

--- marubatsu/service-worker.js
+++ marubatsu/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='yukkuri-asobi-marubatsu-v1';
+const CACHE='yukkuri-asobi-marubatsu-v2';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;}).catch(()=>caches.match('./index.html'))));});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});

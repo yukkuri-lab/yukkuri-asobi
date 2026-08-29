7b9e0e8ef5fe1bc4dc48d819ddf17d9a7a7c0162
yukkuri-lab <tmatsukawa123@gmail.com>
2026-08-29 09:13:54 +0900

    Refresh jindori cache after mode order change

--- jindori/service-worker.js
+++ jindori/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='yukkuri-asobi-jindori-v2';
+const CACHE='yukkuri-asobi-jindori-v3';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
